444efb77ae13e63d85b388ed9ae6d6c11c3aedad
Alex Godino <alex.godino.0403@fernando3martos.com>
2025-06-02 17:52:55 +0200

    Proyecto finalizado

--- Backend/src/database/BBDD.js
+++ Backend/src/database/BBDD.js
@@ -1,24 +1,12 @@
-// require("dotenv").config();
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// module.exports = pool;
-
-// db.js
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost', 
-  database: 'ProyectoFinal',
-  password: 'postgres',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
+
